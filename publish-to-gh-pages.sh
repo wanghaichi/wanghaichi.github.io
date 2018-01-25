d20e7bac514ad5bb4dd383111c8fa7dac8a2387d
Liebes <wanghaichi.twtstudio.com>
2018-01-25 09:49:29 +0800

    update

---
+++ publish-to-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+cd ../
+mv .deploy_git/.git/ ./public/
+cd ./public
+git config user.name  "liebes"
+git config user.email "wanghaichi0403@gmail.com"
+# add commit timestamp
+git add .
+git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+git push --force --quiet "https://${TravisCIToken}@${GH_REF}" master:master
